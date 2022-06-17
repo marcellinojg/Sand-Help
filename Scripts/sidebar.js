a67bf4feaaf73289134220ba374d903353fd5940
marcellinojg <c14200043@john.petra.ac.id>
2022-06-17 17:37:18 +0700

    yes

---
+++ Scripts/sidebar.js
@@ -0,0 +1,19 @@
+//Navbar Function
+function openNav() {
+    document.getElementById("openBtn").style.visibility = "hidden";
+
+
+    document.getElementById("mySidebar").style.width = "20%";
+    document.getElementById("main").style.marginLeft = "20%";
+    if (window.innerWidth <= 600) {
+        document.getElementById("mySidebar").style.width = "100%";
+        document.getElementById("main").style.marginLeft = "100%";
+    }
+}
+
+function closeNav() {
+    document.getElementById("openBtn").style.visibility = "visible";
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+//Navbar Function End
